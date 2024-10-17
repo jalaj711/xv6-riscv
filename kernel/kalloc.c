1456b4c4bdd34ad5ba8932bb86d0337f2d1ab8b6
Jalaj <96870071+jalaj711@users.noreply.github.com>
2024-10-17 15:11:33 +0530

    feat: implement memory freelist using per cpu struct
    The physical pages that are free was being implemented by a
    global kmem struct that was protected by a spinlock. However,
    this causes high lock contention as whenever any of the CPU
    wants to allocate or free memory, it has to hold the lock &
    all other CPUs who also want to allocate/free memory must spin
    & wait for that CPU to finish. This wastes a lot of CPU time.
    
    This commit converts the original kmem struct to an array of the
    kmem struct. The array in indexed by the id of the CPU as
    provided by the `cpuid()` function. Each CPU now maintains its
    own freelist and uses that whenever possible. When it runs out
    of its own memory then it "steals" the a page from the freelist
    of another CPU.
    
    Signed-off-by: Jalaj <96870071+jalaj711@users.noreply.github.com>

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -18,17 +18,23 @@ struct run {
   struct run *next;
 };
 
-struct {
+typedef struct {
   struct spinlock lock;
   struct run *freelist;
   int *ref_count;
   void * pa_start;
-} kmem;
+} kmem_t;
+
+kmem_t cpu_kmem[NCPU];
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for(int i=0;i<NCPU;i++){
+    char name[6] = "kmem_ ";
+    name[5] = '0' + i;
+    initlock(&cpu_kmem[i].lock, name);
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -42,14 +48,24 @@ freerange(void *pa_start, void *pa_end)
   int sz = (pa_end - p) / PGSIZE  * sizeof(int);
   sz = PGROUNDUP(sz);
 
-  kmem.ref_count = (int *)p;
+  // freerange is only called by kinit which is only called once
+  // during boot, by main.c. This is only called on the cpu id 0
+  // so we can directly hardcode [0]
+  cpu_kmem[0].ref_count = (int *)p;
   // we reserve the first few pages for reference count storage
   // so the actual usable physical ram starts from p+sz;
-  kmem.pa_start = p+sz;
-  p = kmem.pa_start;
+  cpu_kmem[0].pa_start = p+sz;
+  p = cpu_kmem[0].pa_start;
   for(; p + PGSIZE <= pa_end; p += PGSIZE) {
-    kmem.ref_count[((char *)p - (char *)kmem.pa_start)/PGSIZE] = 1;
+    cpu_kmem[0].ref_count[((char *)p - (char *)cpu_kmem[0].pa_start)/PGSIZE] = 1;
     kfree(p);
+  }
+
+  // copy the physical address start and ref_count pointer to each CPU
+  // because these will be same for each CPU throughout the program
+  for(int i=1;i<NCPU;i++) {
+    cpu_kmem[i].pa_start = cpu_kmem[0].pa_start;
+    cpu_kmem[i].ref_count = cpu_kmem[0].ref_count;
   }
 }
 
@@ -59,14 +75,15 @@ freerange(void *pa_start, void *pa_end)
 void
 krefcntadd(void *pa, int n) {
   struct run *r;
+  const int cpu_id = cpuid();
   char * pa_start;
-  pa_start = (char *)kmem.pa_start;
+  pa_start = (char *)cpu_kmem[cpu_id].pa_start;
 
   if(((uint64)pa % PGSIZE) != 0 || (char *)pa < pa_start || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
-  acquire(&kmem.lock);
-  int *ref = &kmem.ref_count[((char *)pa - pa_start)/PGSIZE];
+  acquire(&cpu_kmem[cpu_id].lock);
+  int *ref = &cpu_kmem[cpu_id].ref_count[((char *)pa - pa_start)/PGSIZE];
   *ref += n;
 
   if (*ref < 0) {
@@ -76,24 +93,25 @@ krefcntadd(void *pa, int n) {
   if (*ref == 0) {
     memset(pa, 1, PGSIZE);
     r = (struct run*)pa;
-    r->next = kmem.freelist;
-    kmem.freelist = r;
+    r->next = cpu_kmem[cpu_id].freelist;
+    cpu_kmem[cpu_id].freelist = r;
   }
-  release(&kmem.lock);
+  release(&cpu_kmem[cpu_id].lock);
 }
 
 // returns the ref count of the page at pa
 int
 krefcnt(void *pa) {
   char * pa_start;
-  pa_start = (char *)kmem.pa_start;
+  const int cpu_id = cpuid();
+  pa_start = (char *)cpu_kmem[cpu_id].pa_start;
 
   if(((uint64)pa % PGSIZE) != 0 || (char *)pa < pa_start || (uint64)pa >= PHYSTOP)
     panic("kfree");
 
-  acquire(&kmem.lock);
-  int ref = kmem.ref_count[((char *)pa - pa_start)/PGSIZE];
-  release(&kmem.lock);
+  acquire(&cpu_kmem[cpu_id].lock);
+  int ref = cpu_kmem[cpu_id].ref_count[((char *)pa - pa_start)/PGSIZE];
+  release(&cpu_kmem[cpu_id].lock);
 
   return ref;
 }
@@ -109,6 +127,27 @@ kfree(void *pa)
   krefcntadd(pa, -1);
 }
 
+// function to steal the memory from another cpu
+// when this current CPU's memory goes empty
+struct run *
+stealmem()
+{
+  struct run *r;
+  int flag = 0;
+  for (int i=0;i<NCPU;i++) {
+    acquire(&cpu_kmem[i].lock);
+    r = cpu_kmem[i].freelist;
+    if (r) {
+      cpu_kmem[i].freelist = r->next;
+      ++cpu_kmem[i].ref_count[((char *)r - (char *)cpu_kmem[i].pa_start) / PGSIZE];
+      flag = 1;
+    }
+    release(&cpu_kmem[i].lock);
+    if (flag) break;
+  }
+  return r;
+}
+
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
@@ -116,17 +155,24 @@ void *
 kalloc(void)
 {
   struct run *r;
+  const int cpu_id = cpuid();
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  acquire(&cpu_kmem[cpu_id].lock);
+  r = cpu_kmem[cpu_id].freelist;
   if(r){
-    kmem.freelist = r->next;
+    cpu_kmem[cpu_id].freelist = r->next;
     // increment ref count
-    ++kmem.ref_count[((char *)r - (char *)kmem.pa_start) / PGSIZE];
+    ++cpu_kmem[cpu_id].ref_count[((char *)r - (char *)cpu_kmem[cpu_id].pa_start) / PGSIZE];
   }
-  release(&kmem.lock);
+  release(&cpu_kmem[cpu_id].lock);
 
-  if(r)
+  // if our memory is not free then try stealing from some
+  // other cpu. It is important to release our own lock before
+  // trying to steal memory because if we don't then multiple
+  // CPUs trying to steal memory together can easily lead to
+  // a deadlock.
+  if(r || (r = stealmem()))
     memset((char*)r, 5, PGSIZE); // fill with junk
+
   return (void*)r;
 }
